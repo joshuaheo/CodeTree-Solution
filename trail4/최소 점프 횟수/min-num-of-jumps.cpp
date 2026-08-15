0280e743200c66a18cb74d88e4ed403742edc03d
joshuaheo <joshua20030616@gmail.com>
2026-08-15 15:42:55 +0900

    [Passed] 최소 점프 횟수 (2ms, 0MB)

---
+++ trail4/최소 점프 횟수/min-num-of-jumps.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int n, answer = -1;
+void finding(vector<pair<int, int>> &v, int location)
+{
+    if (location == 0)
+    {
+        answer = v[0].second;
+        return;
+    }
+    for (int i = 1; i <= 4; i++)
+    {
+        if(location-i<0)
+        {
+            break;
+        }
+        if (v[location - i].first >= i)
+        {
+            if(v[location-i].second<=v[location].second)
+            {
+                continue;
+            }
+            v[location-i].second=v[location].second+1;
+            finding(v,location-i);
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d", &n);
+    vector<pair<int, int>> v(n, {0, 11});
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &v[i].first);
+    }
+    v[n-1].second=0;
+    finding(v, n - 1);
+    printf("%d", answer);
+    return 0;
+}
