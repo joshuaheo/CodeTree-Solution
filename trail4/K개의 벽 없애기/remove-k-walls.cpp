e5820e10a785339f2ca104c016fdfbbc939406cd
joshuaheo <joshua20030616@gmail.com>
2026-09-03 23:28:21 +0900

    [Passed] K개의 벽 없애기 (2ms, 0MB)

--- trail4/K개의 벽 없애기/remove-k-walls.cpp
+++ trail4/K개의 벽 없애기/remove-k-walls.cpp
@@ -2,94 +2,78 @@
 using namespace std;
 #include <vector>
 #include <queue>
+#include <tuple>
 int dr[4] = {1, 0, -1, 0};
 int dc[4] = {0, 1, 0, -1};
 int n, k;
-vector<pair<int, int>> wall;
-int result = -1;
-int sr, sc, nr, nc;
-void pushing(vector<vector<int>> &v, vector<vector<int>> &visit, queue<pair<int, int>> &q, int row, int col)
+void pushing(vector<vector<int>> &v, vector<vector<vector<int>>> &visit, queue<tuple<int, int, int>> &q, int row, int col, int broken)
 {
     if (row < 0 || row >= n || col < 0 || col >= n)
     {
         return;
     }
-    if (visit[row][col] == 1)
+    if (visit[row][col][broken] == 1)
     {
         return;
     }
-    if (v[row][col] == 1)
+    if(v[row][col]==1)
     {
-        return;
+        if(broken==k)
+        {
+            return;
+        }
+        else{
+            broken++;
+        }
     }
-    q.push({row, col});
-    visit[row][col] = 1;
+    q.push({row, col, broken});
+    visit[row][col][broken] = 1;
 }
-void solving(vector<vector<int>> &v)
+int main()
 {
-    queue<pair<int, int>> q;
-    vector<vector<int>> visit(n, vector<int>(n, 0));
-    q.push({sr - 1, sc - 1});
-    visit[sr - 1][sc - 1] = 1;
+    scanf("%d %d", &n, &k);
+    vector<vector<int>> v(n, vector<int>(n));
+    vector<vector<vector<int>>> visit(n, vector<vector<int>>(n, vector<int>(k + 1, 0)));
+    queue<tuple<int, int, int>> q;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &v[i][j]);
+        }
+    }
+    int sr, sc, nr, nc;
+    scanf("%d %d", &sr, &sc);
+    scanf("%d %d", &nr, &nc);
+    sr--;
+    sc--;
+    nr--;
+    nc--;
+    q.push({sr, sc, 0});
+    visit[sr][sc][0] = 1;
     int cnt = 0;
     while (!q.empty())
     {
         int size = q.size();
         while (size)
         {
-            int row = q.front().first;
-            int col = q.front().second;
+            auto [row, col, broken] = q.front();
+            if (row == nr && col == nc)
+            {
+                printf("%d", cnt);
+                return 0;
+            }
             q.pop();
-            if (row == nr - 1 && col == nc - 1)
-            {
-                result = cnt;
-                return;
-            }
-            if (cnt == result)
-            {
-                return;
-            }
             for (int i = 0; i < 4; i++)
             {
                 int a = row + dr[i];
                 int b = col + dc[i];
-                pushing(v, visit, q, a, b);
+                pushing(v,visit,q,a,b,broken);
             }
             size--;
         }
         cnt++;
     }
-}
-void deleting(int index, int count, vector<vector<int>> &v)
-{
-    if (count == k || index == wall.size())
-    {
-        solving(v);
-        return;
-    }
-    v[wall[index].first][wall[index].second] = 0;
-    deleting(index + 1, count + 1, v);
-    v[wall[index].first][wall[index].second] = 1;
-    deleting(index + 1, count, v);
-}
-int main()
-{
-    scanf("%d %d", &n, &k);
-    vector<vector<int>> v(n, vector<int>(n));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d", &v[i][j]);
-            if (v[i][j] == 1)
-            {
-                wall.push_back({i, j});
-            }
-        }
-    }
-    scanf("%d %d", &sr, &sc);
-    scanf("%d %d", &nr, &nc);
-    deleting(0, 0, v);
-    printf("%d", result);
+    printf("-1");
     return 0;
 }
